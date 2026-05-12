cbd3aa92afc3dcb6e6a74472e7c0dd0486a9692c
RensLm <Rens.Lamfers@Ichthuslyceum.nl>
2026-05-12 08:44:24 +0000

    kleurtjeswinkel

--- shop.php
+++ shop.php
@@ -1,2 +1,24 @@
-<h1>Wedstrijden</h1>
-<a href=index.php>Hoofd pagina</a>
+<!DOCTYPE html>
+
+<html lang="nl">
+
+<head>
+
+<title>Paginatemplate</title>
+
+    <meta charset="utf-8">
+    <meta name="author" content="DaanMidasRens">
+    <meta name="keywords" content="template,HTML,metatags">
+    <meta name="description" content="Dit is de HTML5 template voor iedere pagina">
+    <link rel="stylesheet" type="text/css" href="kleurtjes.css">
+
+</head>
+
+<body>
+
+    <h1>Wedstrijden</h1>
+    <a href=index.php>Hoofd pagina</a>
+
+</body>
+
+</html>
