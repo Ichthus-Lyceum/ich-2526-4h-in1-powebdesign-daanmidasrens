b1cd2063f317aedfa29eb840bd0bff33832caf40
RensLm <Rens.Lamfers@Ichthuslyceum.nl>
2026-05-12 08:40:27 +0000

    css

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<h1>Erecasino Goud</h1>
-<a><img src=plaatjes/logo.png width = "200" height = "200"></a>
-<p><a href=shop.php>Wedstrijden</a></p>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+
+<title>Paginatemplate</title>
+
+    <meta charset="utf-8">
+    <meta name="author" content="DaanMidasRens">
+    <meta name="keywords" content="template,HTML,metatags">
+    <meta name="description" content="Dit is de HTML5 template voor iedere pagina">
+    <link rel="stylesheet" type="text/css" href="kleurtjes.css">
+
+</head>
+
+<body>
+
+    </html>
+    <h1>Erecasino Goud</h1>
+    <a><img src=plaatjes/logo.png width = "200" height = "200"></a>
+    <p><a href=shop.php>Wedstrijden</a></p>
+
+</body>
