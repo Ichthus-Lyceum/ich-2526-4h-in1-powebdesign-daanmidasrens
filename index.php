8e8dae78fec8ff67adfd087b7ca18e4007622340
RensLm <Rens.Lamfers@Ichthuslyceum.nl>
2026-06-02 08:40:22 +0000

    commiteroniratenateronitas

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <meta name="description" content="Dit is de HTML5 template voor iedere pagina">
     <link rel="stylesheet" type="text/css" href="kleurtjes.css">
 
+<body>
+
 </head>
 
 <header>
@@ -17,16 +19,24 @@
     <a><img src=plaatjes/logo.png width = "80" height = "80"></a>
 </header>
 
-<body>
-
+<nav>
     <p><a href=shop.php>Wedstrijden</a></p>
+    <p><a href=shop2.php>Winkel</a></p>
+    <p><a href=gokeuh.php>Gokken</a></p>
+</nav>
 
+<main>
     <p> 
         Welkom bij Erecasino Goud wij zijn een gok website voor de erendivicie. 
         Wij zijn een betrouwbare gok site waar je geld kan verdienen voor mensen van 18+.
         U moet minimaal 10 euro inzetten om te kunnen gokken.
     </p>
+</main>
+
+<footer>
+    <p><a href=overons.php>over ons</a></p>
+</footer>
 
 </body>
 
-    </html>
+    </html
