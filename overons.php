cf5ee58cb0e454e0073ff6d403713c1ce5d88c13
midaskwast <midaskwast1@gmail.com>
2026-06-02 08:02:27 +0000

    overons

---
+++ overons.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+
+
+
+    <meta charset="utf-8">
+    <meta name="author" content="DaanMidasRens">
+    <meta name="keywords" content="template,HTML,metatags">
+    <meta name="description" content="Dit is de HTML5 template voor iedere pagina">
+    <link rel="stylesheet" type="text/css" href="kleurtjes.css">
+
+</head>
+
+<body>
+
+    </html>
+    <h1> Info over ons </h1>
+
+    <p> Wij zijn ere casino goud en 
+       
+    </p>
+
+</body>
