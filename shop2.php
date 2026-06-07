604bb490d2a356279a9b25ad41f15b5a64f11787
midaskwast <midaskwast1@gmail.com>
2026-06-07 13:28:08 +0000

    winkel update

--- shop2.php
+++ shop2.php
@@ -27,7 +27,10 @@
     <p>
         Welkom bij de Erencasino goud zijn merch Winkel. 
         In deze winkel kun je merch vinden van de erendevicie en van bekende clubs.
-        Na een bestelling van 50 euro zijn er geen verzend kosten.
+         Mocht het shirt wat u nou net wild uitverkocht zijn geen zorgen. Wij beheeren onze vooraad en zorgen dat uitverkochte producten nooit langer dan een week uit vooraad zijn.
+       Ook zal hier specifiek per wedstrijd een aantal nieuwe producten beschikbaar komen op basis van de club.
+       We zorgen ook dat er altijd variatie zal zijn van clubs en producten zoals shirts, mokken en andere spullen. 
+         U zal ook vanaf een bestelling van 50 euro bij ons geen verzend kosten meer hoeven te betalen.
     </p>
 
 </main>
